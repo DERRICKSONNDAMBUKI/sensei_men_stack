39cbf8e34b48fbd72df581e48cf33c04ac72cea3
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-02-06 02:03:03 +0300

    express embeddable js(ejs) commit

--- app.js
+++ app.js
@@ -10,16 +10,20 @@ app.set('view engine','ejs')
 
 // routing
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "pages/index.html"));
+//   res.sendFile(path.resolve(__dirname, "pages/index.html"));
+res.render('index')
 });
 app.get("/about", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "pages/about.html"));
+//   res.sendFile(path.resolve(__dirname, "pages/about.html"));
+res.render('about')
 });
 app.get("/contact", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "pages/contact.html"));
+//   res.sendFile(path.resolve(__dirname, "pages/contact.html"));
+res.render('contact')
 });
 app.get("/post", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "pages/post.html"));
+//   res.sendFile(path.resolve(__dirname, "pages/post.html"));
+res.render('post')
 });
 
 app.listen(3000, () => {
