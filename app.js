bc43f31dc96aba544e3d1ec4209f5cc5c8e26362
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-02-06 00:37:14 +0300

    express sendFile commit

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
-const app = express() //calls express function start new express app
+const app = express(); //calls express function start new express app
 
 // routing
-app.get('/',(req,res)=>{
-    res.json({
-        name:'Sensei'
-    })
-})
+app.get("/", (req, res) => {
+  res.json({
+    name: "Sensei",
+  });
+});
 
-app.get('/home',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'index.html'))
-})
+app.get("/home", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "index.html"));
+});
 
+app.get("/about", (req, res) => {
+  // called when request to /about comes in
+  res.sendFile(path.resolve(__dirname, "about.html"));
+});
 
-app.listen(3000,()=>{
-    console.log('app listening at port 3000');
-})
+app.get("/contact", (req, res) => {
+  //called when request to /contact comes
+  res.sendFile(path.resolve(__dirname, "contact.html"));
+});
+
+app.listen(3000, () => {
+  console.log("app listening at port 3000");
+});
