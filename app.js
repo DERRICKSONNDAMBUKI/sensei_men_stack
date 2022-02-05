cd3de2ad0833d884254b5ff8577e72d5d3347233
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-02-06 00:05:00 +0300

    nodeJs commit

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
-
-
 const http = require('http')
+const fs  = require('fs')
+
+// files
+const homePage = fs.readFileSync('./index.html')
+const aboutPage = fs.readFileSync('./about.html')
+const contactPage = fs.readFileSync('./contact.html')
+const notFound = fs.readFileSync('./notfound.html')
 
 const server = http.createServer((req,res)=>{
     console.log(req.url);
     // res.end('Hello Sensei!')
     if (req.url === '/about') {
-        res.end('The about page')
+        res.end(aboutPage)
     } else if(req.url==='/contact'){
-        res.end('The contact page')
+        res.end(contactPage)
     }else if (req.url==='/') {
-        res.end('The home page')
+        res.end(homePage)
     } else {
         res.writeHead(404)
-        res.end('Page not found')
+        res.end(notFound)
     }
 })
 
