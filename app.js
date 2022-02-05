e52df6a2aec1f447ef963fa88dbda308e8dff0b7
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-02-06 01:40:58 +0300

    express embeddable js(ejs) commit

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require("path");
 
 const app = express(); //calls express function start new express app
+
+// middlewares
 app.use(express.static("public"));
 
 // routing
