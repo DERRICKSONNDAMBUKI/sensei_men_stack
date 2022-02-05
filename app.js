ff829aa15d925a6489922fbd65f1a47ba81f5459
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-02-05 23:42:07 +0300

    init commit

--- app.js
+++ app.js
@@ -4,7 +4,17 @@ const http = require('http')
 
 const server = http.createServer((req,res)=>{
     console.log(req.url);
-    res.end('Hello Sensei!')
+    // res.end('Hello Sensei!')
+    if (req.url === '/about') {
+        res.end('The about page')
+    } else if(req.url==='/contact'){
+        res.end('The contact page')
+    }else if (req.url==='/') {
+        res.end('The home page')
+    } else {
+        res.writeHead(404)
+        res.end('Page not found')
+    }
 })
 
 server.listen(3000)
