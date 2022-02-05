184a98c6f2872bb340f9eff9ba5ebe2ca379cd22
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-02-06 00:29:43 +0300

    express sendFile commit

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express');
+const path = require('path');
 
 const app = express() //calls express function start new express app
 
+// routing
 app.get('/',(req,res)=>{
     res.json({
         name:'Sensei'
     })
 })
 
+app.get('/home',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'index.html'))
+})
 
 
 app.listen(3000,()=>{
