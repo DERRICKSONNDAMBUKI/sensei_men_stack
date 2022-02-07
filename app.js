ef785985001772e7405b49dd3666ff48ad723071
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-02-07 13:11:10 +0300

    new post commit

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require("express");
-const path = require("path");
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
 
 const app = express(); //calls express function start new express app
 
 // middlewares
 app.use(express.static("public"));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
 app.set('view engine','ejs')
 
 
